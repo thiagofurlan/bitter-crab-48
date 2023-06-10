fed1ab364b3c15fcfc4617f8cba8e3a424af2d07
Thiago Furlan <thiago.sfurlan@outlook.com>
2023-06-10 15:03:07 -0300

    go

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,5 +1,34 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+interface User {
+  login: string;
+  name: string;
+  avatar_url: string;
+}
+
+export const handler: Handlers<User | null> = {
+  async GET(_, ctx) {
+    const { name } = ctx.params;
+    const resp = await fetch(`https://api.github.com/users/${name}`);
+    console.log(resp.status, name);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const user: User = await resp.json();
+    return ctx.render(user);
+  },
+};
+
+export default function Page({ data }: PageProps<User | null>) {
+  if (!data) {
+    return <h1>User not found</h1>;
+  }
+
+  return (
+    <div>
+      <img src={data.avatar_url} width={64} height={64} />
+      <h1>{data.name}</h1>
+      <p>{data.login}</p>
+    </div>
+  );
 }
